3aafd452a8b0e0e5eb0b2c2da8684fdba3f2664c
Angusvencent <115913884+Angusvencent@users.noreply.github.com>
2023-02-21 01:41:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ email = os.environ.get('EMAIL')
 passwd = os.environ.get('PASSWD')
 # server酱
 SCKEY = os.environ.get('SCKEY')
+#配置cookie
+cookie = os.environ.get('COOKIE')
 
 login_url = '{}/auth/login'.format(url)
 check_url = '{}/user/checkin'.format(url)
 
 
 header = {
-        'cookie':'lang=zh-cn; _ga=GA1.2.1723288332.1676846711; _gid=GA1.2.1475360412.1676846711; uid=917985; email=angusvencent@gmail.com; key=07d46b5d38c4f1f5d9e54c81efcecf8b6354db012f097; ip=a02c0f377e30dec1b22da4055ed19359; expire_in=1677454513'
+        'cookie':cookie,
         'origin': url,
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
