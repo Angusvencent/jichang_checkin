45c4042ba11b4777d2ff1d80e3f262a4ded94045
Angusvencent <115913884+Angusvencent@users.noreply.github.com>
2023-02-21 02:05:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ header = {
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
 # data = {
-        'email': email,
-        'passwd': passwd
+ #       'email': email,
+ #       'passwd': passwd
 }
 try:
    # print('进行登录...')
