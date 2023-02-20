f2987d388f47bdc2d0249a6dbf3f08797d92ec1c
Angusvencent <115913884+Angusvencent@users.noreply.github.com>
2023-02-21 01:30:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ check_url = '{}/user/checkin'.format(url)
 
 
 header = {
+        'cookie':'lang=zh-cn; _ga=GA1.2.1723288332.1676846711; _gid=GA1.2.1475360412.1676846711; uid=917985; email=angusvencent@gmail.com; key=07d46b5d38c4f1f5d9e54c81efcecf8b6354db012f097; ip=a02c0f377e30dec1b22da4055ed19359; expire_in=1677454513'
         'origin': url,
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
@@ -23,9 +24,9 @@ data = {
         'passwd': passwd
 }
 try:
-    print('进行登录...')
-    response = json.loads(session.post(url=login_url,headers=header,data=data).text)
-    print(response['msg'])
+   # print('进行登录...')
+   # response = json.loads(session.post(url=login_url,headers=header,data=data).text)
+   # print(response['msg'])
     # 进行签到
     result = json.loads(session.post(url=check_url,headers=header).text)
     print(result['msg'])
